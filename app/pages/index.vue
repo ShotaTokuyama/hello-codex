23aaa7f8f6d3ba56cde9b2dcc5a0bfab5c169cb3
ShotaTokuyama <tokush1031@gmail.com>
2025-11-01 12:01:37 +0900

    counterApp

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,28 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const counter = ref(0);
+
+const inclease = () => {
+  counter.value += 1;
+};
+
+const declease = () => {
+  if (counter.value === 0) return;
+  counter.value -= 1;
+};
+
+const reset = () => {
+  counter.value = 0;
+};
+</script>
 
 <template>
-  <UButton>Button</UButton>
+  <UHeader />
+  <UContainer class="py-12">
+    <h2>{{ counter }}</h2>
+    <div class="flex gap-x-3">
+      <UButton @click="inclease">inclease</UButton>
+      <UButton @click="declease">declease</UButton>
+      <UButton @click="reset">Reset</UButton>
+    </div>
+  </UContainer>
 </template>
